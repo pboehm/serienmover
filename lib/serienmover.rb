81bfe3b98ebd8da1403e6233b0efa5364e676ee6
Philipp Böhm <philipp-boehm@live.de>
2012-04-08 23:08:47 +0200

    reverted version file change

--- lib/serienmover.rb
+++ lib/serienmover.rb
@@ -1,8 +1,8 @@
 require "serienmover/series_store.rb"
 require "serienmover/episode.rb"
+require "serienmover/version.rb"
 require "serienrenamer"
 
 module Serienmover
 
-  VERSION = "0.1.0"
 end
---
+++ lib/serienmover/version.rb
@@ -0,0 +1,3 @@
+module Serienmover
+  VERSION = "0.1.0"
+end
